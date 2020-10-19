52bd5ba2c3360e48da61b4f060c7f09ed620eb17
IcaroLettieri <contact@icarolettieri.com>
2020-10-19 17:52:46 -0300

    Add arquivos server Adonis

---
+++ airbnb-server/start/routes.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+/** Rotas publicas */
+Route.get('/', () => {return { Message: 'Welcome to API' }})
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+Route.get('images/:path', 'ImageController.show')
+
+
+/** Rotas autenticadas */
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
+
+Route.post('properties/:id/images', 'ImageController.store')
+  .middleware('auth')
